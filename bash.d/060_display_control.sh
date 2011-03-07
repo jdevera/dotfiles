d85bf420129ce8deb043bfe0441daf9a26b945dc
Jacobo de Vera <devel@jacobodevera.com>
2011-03-07 23:42:58 +0000

    bash: new display setting (two monitors)

--- bash.d/060_display_control.sh
+++ bash.d/060_display_control.sh
@@ -13,6 +13,7 @@ aurora)
     alias displaylcd='xrandr --output LVDS1 --mode 1440x900 && xrandr --output TV1   --off --output VGA1 --off'
     alias displaytv='xrandr  --output TV1   --mode 1024x768 && xrandr --output LVDS1 --off --output VGA1 --off'
     alias displayvga='xrandr --output VGA1  --mode 1920x1200 && xrandr --output LVDS1 --off --output TV1  --off'
+    alias displayboth='xrandr --output VGA1 --primary && xrandr --output VGA1 --auto --pos 0x0 --output LVDS1 --auto --right-of VGA1'
     ;;
 helicon|solaria)
     # Nvidia cards
