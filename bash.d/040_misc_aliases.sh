5d300d744bb433874ec28dbf8081c1f00928cd34
Jacobo de Vera <devel@jacobodevera.com>
2011-07-08 12:37:23 +0100

    Bash: Add an alias to clear the system cache

--- bash.d/040_misc_aliases.sh
+++ bash.d/040_misc_aliases.sh
@@ -32,7 +32,7 @@ alias ..?='show_parent_dirs'
 alias du1='du --max-depth 1'
 alias dumd='du --max-depth'
 
-
+alias clearcache='sudo sync; sudo echo 3 | sudo tee /proc/sys/vm/drop_caches > /dev/null'
 
 # vim: ft=bash fdm=marker expandtab ts=3 sw=3 :
 
