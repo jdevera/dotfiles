6261d6446e53f75af155a787abf50efe3db001fe
Jacobo de Vera <devel@jacobodevera.com>
2011-10-24 12:27:28 +0100

    Bash: Disable cdable_vars setting
    This setting has been handy for a while but I have now switched to using
    a script to bookmark directories and quickly change into them, so this
    is of no use any more for me and only introduces noise when doing tab
    completion on the cd command.

--- bash.d/050_misc_config.sh
+++ bash.d/050_misc_config.sh
@@ -184,11 +184,6 @@ pathprepend "$HOME/.cabal/bin/"
 pathprepend "$DDROPBOX/todo"
 
 
-# an argument to the cd builtin command that is not a directory is assumed to
-# be the name of a variable whose value is the directory to change to.
-shopt -s cdable_vars
-
-
 # Check the window size after each command and, if necessary, update the
 # values of LINES and COLUMNS.
 shopt -s checkwinsize
