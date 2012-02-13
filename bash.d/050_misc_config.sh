67a4d7249155bdd770ed4e70f8d142faef8cb437
Jacobo de Vera <devel@jacobodevera.com>
2012-02-13 14:14:22 +0000

    Bash: Some adjustments to history configuration
    * Increase history size and history file size.
    * Ignore directory changing (cd and cdd) commands.
    * Stop ignoring duplicates for a while, as I want to do some frequency
      analysis.

--- bash.d/050_misc_config.sh
+++ bash.d/050_misc_config.sh
@@ -169,17 +169,56 @@ default_prompt_color()
 # Section: History {{{
 #############################################################################
 
-# Don't put duplicate lines in the history and also don't save lines
-# that begin with spaces
-HISTCONTROL=ignoreboth
+# HISTCONTROL
+# ===========
+# A colon-separated list of values controlling how commands are saved on the
+# history list. If the list of values includes ignorespace, lines which begin
+# with a space character are not saved in the history list. A value of
+# ignoredups causes lines matching the previous history entry to not be saved.
+# A value of ignoreboth is shorthand for ignorespace and ignoredups. A value of
+# erasedups causes all previous lines matching the current line to be removed
+# from the history list before that line is saved. Any value not in the above
+# list is ignored. If HISTCONTROL is unset, or does not include a valid value,
+# all lines read by the shell parser are saved on the history list, subject to
+# the value of HISTIGNORE. The second and subsequent lines of a multi-line
+# compound command are not tested, and are added to the history regardless of
+# the value of HISTCONTROL.
+HISTCONTROL=ignorespace
 
-# Append to the history file, don't overwrite it
+# HISTSIZE
+# ========
+# The number of commands to remember in the command history. The default value
+# is 500.
+HISTSIZE=2000
+
+# HISTFILESIZE
+# ============
+# The maximum number of lines contained in the history file. When this variable
+# is assigned a value, the history file is truncated, if necessary, by removing
+# the oldest entries, to contain no more than that number of  lines. The
+# default value is 500. The history file is also truncated to this size after
+# writing it when an interactive shell exits.
+HISTFILESIZE=10000
+
+# HISTIGNORE
+# ==========
+# A colon-separated list of patterns used to decide which command lines should
+# be saved on the history list. Each pattern is anchored at the beginning of
+# the line and must match the complete line (no implicit `*' is appended). Each
+# pattern is tested against the line after the checks specified by HISTCONTROL
+# are applied. In addition to the normal shell pattern matching characters, `&'
+# matches the previous history line. `&' may be escaped using a backslash; the
+# backslash is removed before attempting a match. The second and subsequent
+# lines of a multi-line compound command are not tested, and are added to the
+# history regardless of the value of HISTIGNORE.
+HISTIGNORE="ls:cd:cd *:pwd:cdd *"
+
+# shopt: histappend
+# =================
+# If set, the history list is appended to the file named by the value of the
+# HISTFILE variable when the shell exits, rather than overwriting the file.
 shopt -s histappend
 
-# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
-HISTSIZE=1000
-HISTFILESIZE=2000
-HISTIGNORE="ls:cd:pwd"
 
 #############################################################################
 # }}}   
