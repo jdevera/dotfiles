7278ca4e0e130531dbaead6bd28081864ffcda1d
Jacobo de Vera <devel@jacobodevera.com>
2013-02-23 22:35:13 +0000

    Bash: Ignore p command in shell history

--- bash.d/050_misc_config.sh
+++ bash.d/050_misc_config.sh
@@ -205,7 +205,7 @@ HISTFILESIZE=10000
 # backslash is removed before attempting a match. The second and subsequent
 # lines of a multi-line compound command are not tested, and are added to the
 # history regardless of the value of HISTIGNORE.
-HISTIGNORE="ls:cd:cd *:pwd:cdd *"
+HISTIGNORE="ls:cd:cd *:pwd:cdd *:p"
 
 # shopt: histappend
 # =================
