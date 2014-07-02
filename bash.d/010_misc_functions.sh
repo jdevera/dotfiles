83eba6b240a49e3dc96d3e1e3c89800ea863e135
Jacobo de Vera <devel@jacobodevera.com>
2014-07-02 16:30:38 +0200

    Bash: Add function origin to the code function

--- bash.d/010_misc_functions.sh
+++ bash.d/010_misc_functions.sh
@@ -88,6 +88,7 @@ function code()
          ;;
       function)
          echo "$1 is a function"
+         find_function $1 | awk '{ printf("Defined in: %s +%d\n", $3, $2) }'
          builtin declare -f "$1" | hless
          ;;
       builtin | keyword)
@@ -109,6 +110,7 @@ function code()
          ;;
    esac
 }
+complete -c code
 
 
 function extract()
