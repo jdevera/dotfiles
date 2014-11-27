d07795c4d5509cedaac74f7f893dddf1b28be398
Jacobo de Vera <devel@jacobodevera.com>
2014-11-27 16:40:47 +0100

    Bash: Add pbcopy and pbpaste aliases
    pbcopy will copy stdin to the clipboard
    pbpaste will dump the clipboard contents to stdout

--- bash.d/040_misc_aliases.sh
+++ bash.d/040_misc_aliases.sh
@@ -65,5 +65,8 @@ complete -o bashdefault -o default -o nospace -F _git tit
 alias jo=jrnl
 alias jot='jrnl --export text | less -RMSFiX'
 
+alias pbcopy='xsel --clipboard --input'
+alias pbpaste='xsel --clipboard --output'
+
 # vim: ft=sh fdm=marker expandtab ts=3 sw=3 :
 
