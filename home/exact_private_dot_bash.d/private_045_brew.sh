898ecd3e333d3ff403c958fa8f097059cb8a061a
Jacobo de Vera <devel@jacobodevera.com>
2025-09-27 21:53:07 +0100

    Brew: Go back to environment to configure Homebrew
    I could not make the env file to work and it seems poorly documented.

--- home/exact_private_dot_bash.d/private_045_brew.sh
+++ home/exact_private_dot_bash.d/private_045_brew.sh
@@ -1,5 +1,29 @@
 #!/usr/bin/env bash
 
+# Homebrew configuration {{{
+# ----------------------
+
+# If set, do not print any hints about changing Homebrew’s behaviour with
+# environment variables.
+export HOMEBREW_NO_ENV_HINTS=1
+
+# Run brew update once every $HOMEBREW_AUTO_UPDATE_SECS seconds before some
+# commands, e.g. brew install, brew upgrade or brew tap. Alternatively, disable
+# auto-update entirely with $HOMEBREW_NO_AUTO_UPDATE.
+#
+# Default: 86400 (24 hours), 3600 (1 hour) if a developer command has been run
+# or 300 (5 minutes) if $HOMEBREW_NO_INSTALL_FROM_API is set.
+export HOMEBREW_AUTO_UPDATE_SECS=604800  # 1 week
+
+# If set, do not send analytics data to Homebrew’s servers.
+export HOMEBREW_NO_ANALYTICS=1
+
+# If set, brew bundle dump will not include VSCode (and forks/variants)
+# extensions.
+export HOMEBREW_BUNDLE_DUMP_NO_VSCODE=1
+# }}}
+
+
 BREW_PATH=
 for candidate in \
     "/opt/homebrew/bin/brew" \
